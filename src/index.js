5153132af5927314084605336eca111161736b96
Alina-github <alinarudkovskaa@gmail.com>
2021-03-01 19:03:14 +0300

    fix: fix incorrect decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) { 
+    return array.sort((a,b) => a - b)[0];
+  } else { 
+    return 0;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+  if (Array.isArray(array) && array.length) { 
+    return array.sort((a,b) => a - b)[array.length - 1];
+  } else { 
+    return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  
+   if (Array.isArray(array) && array.length) {  let totalSum = array.reduce(function(sum,current){return sum + current});
+   return totalSum/array.length;
+  } else { 
+    return 0;
+  }
 }
